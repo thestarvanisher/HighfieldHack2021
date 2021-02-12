bf8527de154c9f2125f2804479744556cc8e49f1
Joe Willmott <joe@joewillmott.co.uk>
2021-02-12 18:59:38 +0000

    Basic Debate Models

--- HighfieldHack2021/apps/core/admin.py
+++ HighfieldHack2021/apps/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from HighfieldHack2021.apps.core.models import Debate, TextArgument
+
+
+class DebateAdmin(admin.ModelAdmin):
+    model = Debate
+    list_display = ("title", "description", "owner", "posted_at", "expires_at")
+    list_filter = ("title", "owner")
+    search_fields = ("title", "owner")
+
+
+class TextArgumentAdmin(admin.ModelAdmin):
+    model = TextArgument
+    list_display = ("title", "description", "owner", "debate", "posted_at")
+    list_filter = ("title", "owner", "debate")
+    search_fields = ("title", "owner", "debate")
+
+
+admin.site.register(Debate, DebateAdmin)
+admin.site.register(TextArgument, TextArgumentAdmin)
