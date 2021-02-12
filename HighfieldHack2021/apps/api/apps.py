ad136e83ff9fef0c3b67de23589bda90ad4a1ede
Joe Willmott <joe@joewillmott.co.uk>
2021-02-12 17:47:46 +0000

    Initial Commit

---
+++ HighfieldHack2021/apps/api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    name = "HighfieldHack2021.apps.api"
---
+++ HighfieldHack2021/apps/core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    name = "HighfieldHack2021.apps.core"
