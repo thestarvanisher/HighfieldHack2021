e9c6b00c0e6287b3a47163f26a94d4f554887bb0
Joe Willmott <joe@joewillmott.co.uk>
2021-02-12 21:38:58 +0000

    Polls

--- HighfieldHack2021/apps/core/admin.py
+++ HighfieldHack2021/apps/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from HighfieldHack2021.apps.core.models import Debate, TextArgument
+from HighfieldHack2021.apps.core.models import Debate, DebateTextArgument, DebateVote, Poll, ChoiceVote, Choice
 
 
 class DebateAdmin(admin.ModelAdmin):
@@ -11,12 +11,42 @@ class DebateAdmin(admin.ModelAdmin):
     search_fields = ("title", "owner")
 
 
-class TextArgumentAdmin(admin.ModelAdmin):
-    model = TextArgument
+class DebateTextArgumentAdmin(admin.ModelAdmin):
+    model = DebateTextArgument
     list_display = ("title", "description", "owner", "debate", "posted_at")
     list_filter = ("title", "owner", "debate")
     search_fields = ("title", "owner", "debate")
 
 
+class DebateVoteAdmin(admin.ModelAdmin):
+    model = DebateVote
+    list_display = ("debate", "owner")
+    list_filter = ("debate", "owner")
+    search_fields = ("debate", "owner")
+
+
+class PollAdmin(DebateAdmin):
+    model = Poll
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    model = DebateTextArgument
+    list_display = ("title", "poll")
+    list_filter = ("title", "poll")
+    search_fields = ("title", "poll")
+
+
+class ChoiceVoteAdmin(admin.ModelAdmin):
+    model = DebateTextArgument
+    list_display = ("choice", "owner")
+    list_filter = ("choice", "owner")
+    search_fields = ("choice", "owner")
+
+
 admin.site.register(Debate, DebateAdmin)
-admin.site.register(TextArgument, TextArgumentAdmin)
+admin.site.register(DebateTextArgument, DebateTextArgumentAdmin)
+admin.site.register(DebateVote, DebateVoteAdmin)
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(ChoiceVote, ChoiceVoteAdmin)
--- HighfieldHack2021/apps/core/models.py
+++ HighfieldHack2021/apps/core/models.py
@@ -1,19 +1,51 @@
 from django.conf import settings
 from django.db import models
 
-
 # Create your models here.
+from HighfieldHack2021.apps.core.helpers import date_in_future
+
+
 class Debate(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     posted_at = models.DateTimeField(auto_now=True)
-    expires_at = models.DateTimeField()
+    expires_at = models.DateTimeField(validators=[date_in_future])
 
 
-class TextArgument(models.Model):
+class DebateTextArgument(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    debate = models.ForeignKey(Debate, on_delete=models.CASCADE)
+    debate = models.ForeignKey(Debate, on_delete=models.CASCADE, related_name="debate_arguments")
     posted_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("title", "debate")
+
+
+class Poll(Debate):
+    pass
+
+
+class Choice(models.Model):
+    title = models.CharField(max_length=50)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name="choices")
+
+    class Meta:
+        unique_together = ("title", "poll")
+
+
+class ChoiceVote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name="choice_votes")
+
+    class Meta:
+        unique_together = ("choice", "owner")
+
+
+class DebateVote(models.Model):
+    debate = models.ForeignKey(Debate, on_delete=models.CASCADE, related_name="debate_votes")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("debate", "owner")
