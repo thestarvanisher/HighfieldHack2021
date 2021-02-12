b31778778174e53d4386f8a64d18d6deea120bd6
edgeboyo <mkolybko@gmail.com>
2021-02-12 19:25:05 +0000

    Login template

--- HighfieldHack2021/frontend/src/App.vue
+++ HighfieldHack2021/frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
---
+++ HighfieldHack2021/frontend/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Sign In</h3>
+
+            <div class="form-group">
+                <label>Username</label>
+                <input type="username" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
