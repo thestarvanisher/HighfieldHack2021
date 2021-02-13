a58f61973a28afbe050f0ad30ba5626660eaa545
Joe Willmott <joe@joewillmott.co.uk>
2021-02-13 06:24:52 +0000

    Forms

--- HighfieldHack2021/apps/debates/forms.py
+++ HighfieldHack2021/apps/debates/forms.py
@@ -1,23 +1,32 @@
 from django.forms import forms
 
-from HighfieldHack2021.apps.core.models import Debate, Poll, PollChoice
+from HighfieldHack2021.apps.core.models import Debate, Poll, PollChoice, PollChoiceVote, DebateTextArgument
 
 
 class DebateForm(forms.Form):
     class Meta:
         model = Debate
+        fields = ("title", "description", "expires_at")
 
 
-class ChoiceVote(forms.Form):
+class PollChoiceVoteForm(forms.Form):
     class Meta:
-        model = Debate
+        model = PollChoiceVote
+        fields = ("choice",)
 
 
 class PollForm(forms.Form):
     class Meta:
         model = Poll
+        fields = ("title", "description", "expires_at")
 
 
 class PollChoiceForm(forms.Form):
     class Meta:
         model = PollChoice
+
+
+class DebateTextArgumentForm(forms.Form):
+    class Meta:
+        model = DebateTextArgument
+        fields = ("title", "description")
